1828123a1746606930f1925533382f9cb0cea1c8
gsngdavid <gsngdavid@gmail.com>
2024-03-06 11:12:35 +0200

    feat: add login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,45 +1,24 @@
-"use client";
-
 import AuthInput from "../../../components/ui/AuthInput";
 import Button from "../../../components/ui/Button";
 
-export default function SignupPage() {
+export default function LoginPage() {
   return (
     <main className="space-x px-10">
       <h1 className="text-5xl font-bold">
-        New <span className="block">Account</span>
+        Welcome <span className="block">back!</span>
       </h1>
       <form className="flex flex-col gap-y-4 mt-10">
-        <AuthInput
-          type="text"
-          id="firstName"
-          name="firstName"
-          placeholder="First name"
-        />
-        <AuthInput
-          type="text"
-          id="lastName"
-          name="lastName"
-          placeholder="Last name"
-        />
         <AuthInput
           type="text"
           id="username"
           name="username"
           placeholder="Username"
         />
-        <AuthInput type="email" id="email" name="email" placeholder="Email" />
         <AuthInput
           type="password"
           id="password"
           name="password"
           placeholder="Password"
-        />
-        <AuthInput
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          placeholder="Confirm password"
         />
         <div className="flex flex-col mt-5">
           <Button>Submit</Button>
